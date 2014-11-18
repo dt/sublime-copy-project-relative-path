94427258290b4a189a938a48094c458c2d9e631d
David Taylor <tinystatemachine@gmail.com>
2014-11-18 23:29:00 +0000

    avoid name clashes with similar but different existing plugin

--- CopyRelativePath.py
+++ CopyRelativePath.py
@@ -3,7 +3,7 @@
 import os
 import sublime, sublime_plugin
 
-class CopyRelativePathCommand(sublime_plugin.TextCommand):
+class CopyProjectRelativePathCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     cur = self.view.file_name() or ''
     proj = sublime.active_window().project_file_name() or ''
